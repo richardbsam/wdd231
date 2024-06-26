586f0c04ddcb9412768d7175761bf724fac188cc
richardbsam <richardbonifacesam@gmail.com>
2024-06-26 12:58:56 +0100

    navigation updated

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -137,6 +137,19 @@ document.addEventListener('DOMContentLoaded', fetchMemberData);
 
 
 
+//Navigation
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
+
+
+
 
 
 
